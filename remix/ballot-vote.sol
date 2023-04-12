76be9669caab1920ebb2cd1a679987df2854ba85
michaelorina <orinamichael4@gmail.com>
2023-04-12 11:22:43 +0300

    create voters struct

--- remix/ballot-vote.sol
+++ remix/ballot-vote.sol
@@ -4,9 +4,18 @@ contract Ballot{
 
     //All code goes here
 
+    //struct is a method to create and store your data types
+
     struct Proposal {
         bytes32 name; //name of each proposal
         uint voteCount; //number of accumulated votes
+    }
+
+    //voters: voted = bool, access to vote = uint, vote index = uint
+    struct Voters {
+        uint vote;
+        bool voted;
+        uint wheight;
     }
     
 
